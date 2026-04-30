026714bdfb9d82ef6bb995d423ec76aea9e29c3d
Binaya764 <bigyanlimbu179@gmail.com>
2026-04-30 21:07:19 +0545

    css file loading fixed

--- index.js
+++ index.js
@@ -7,33 +7,41 @@ const server = http.createServer((req,res) => {
     if(req.url === '/'){
         filePath = 'index.html';
 
-    }else if (req.url === 'about'){
+    }else if (req.url === '/about'){
         filePath = 'about.html';
 
-    }else if(req.url === 'contact-me'){
+    }else if(req.url === '/contact-me'){
         filePath = 'contact-me.html';
-        
-    }else if(req.url === 'index.js'){
+
+    }else if(req.url === '/index.js'){
         filePath = 'index.js';
-
-
     }
-    else if(req.url === 'style.css'){
+
+    else if(req.url === '/style.css'){
         filePath = 'style.css';
     }
     else{
         filePath = '404.html';
     }
 
-fs.readFile(path.join(__dirname,filePath),(err,content)=>{
-    if(err){
+fs.readFile(path.join(__dirname, filePath), (err, content) => {
+    if (err) {
         res.writeHead(500);
         res.end('Error loading page');
-    }
-    else{
-        const statusCode = filePath === '404.html' ?404 : 200;
-        res.writeHead(statusCode,{
-            'Content-Typs': 'text/html'
+    } else {
+        const statusCode = filePath === '404.html' ? 404 : 200;
+        
+        // 1. Determine the correct Content-Type
+        let contentType = 'text/html';
+        if (filePath.endsWith('.css')) {
+            contentType = 'text/css';
+        } else if (filePath.endsWith('.js')) {
+            contentType = 'text/javascript';
+        }
+
+        // 2. Use the dynamic contentType variable here
+        res.writeHead(statusCode, {
+            'Content-Type': contentType
         });
         res.end(content, 'utf-8');
     }
