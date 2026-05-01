9884f91d2668faa9a69e25b9960e6fe1554a6212
Binaya764 <bigyanlimbu179@gmail.com>
2026-05-01 21:16:39 +0545

    button linked to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let print =console.log("script js connected");
+console.log("script js connected");
+
+let btn = document.querySelector(".navbar_btn");
+
+if (btn) {
+    btn.addEventListener("click", () => {
+        console.log("button was clicked");
+        
+    });
+} else {
+    console.warn("Button with class .navbar_btn was not found on this page.");
+}
