c89071dfee0676f7867bcfec9074bea5ac4bae63
Binaya764 <bigyanlimbu179@gmail.com>
2026-05-06 23:13:41 +0545

    routes added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const authorRouter = require("./routes/authorRouter");
+const bookRouter = require("./routes/bookRouter");
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/index", indexRouter);
+
+/*
 app.use(express.static('public'));
 app.get("/",(req,res)=>
 {
@@ -13,6 +22,7 @@ app.get("/about", (req,res)=>{
 app.get("/contact-me",(req,res)=>{
     res.sendFile(path.join(__dirname, "contact-me.html"));
 });
+*/
 
 const PORT = 3000;
 app.listen(PORT, (error)=>{
